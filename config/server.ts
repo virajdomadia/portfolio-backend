ef68c9fccb66020aa6c6ffb23818db17f12088bf
Viraj <virajdomadia32@gmail.com>
2025-01-19 15:50:06 +0530

    updated CORS

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,8 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Listen on all network interfaces
+  port: env.int('PORT', 1337),   // Default port 1337
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('SERVER_URL', 'https://portfolio-backend-311n.onrender.com'), // Add this line to set your public URL
 });
